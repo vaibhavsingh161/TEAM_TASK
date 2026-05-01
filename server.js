e433876af3ee48f83531243d2174b18c71cd8a84
Vaibhav Pratap <pratapvaibhav075@gmail.com>
2026-05-02 01:17:31 +0530

    fix: update CORS config

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://team-task-manager-xi-eight.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
